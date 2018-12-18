d7269e6ed9682d01cfdec93b03a0ae02cf4dc4cb
Владислав Бородин <vb@rocketstation.ru>
2018-12-18 13:51:44 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Header from './components/header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('layout'));
+
 
 serviceWorker.unregister();
