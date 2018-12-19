2cce64d9c54702f7b881dd48d58ce3fe2f52a05d
Vladislav <vlad.yurash@gmail.com>
2018-12-19 17:18:30 +0300

    header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import './index.css';
+import './index.sass';
+import logo from './logo.svg'
+import user from './user.png'
 
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
+    <div className="container">
+      <div className="header">
+        <div className="header__logo">
+            <img src={logo} alt="logo"></img>
+        </div>
 
+        <nav className="header__nav">
+          <ul>
+            <li><a className="header__link">About me</a></li>
+            <li><a className="header__link">Relationships</a></li>
+            <li><a className="header__link">Requirements</a></li>
+            <li><a className="header__link">Users</a></li>
+            <li><a className="header__link">Sign Up</a></li>
+          </ul>
+        </nav>
 
+        <div className="header__user">
+            <div className="header__user-info">
+                <p className="header__user-name">Superstar</p>
+                <p className="header__user-mail">Superstar@gmail.com</p>
+            </div>
+            <div className="header__user-logo">
+                <img src={user} alt="user"></img>
+            </div>
+            <a className="header__user-exit"></a>
+
+        </div>
       </div>
+
+    </div>
 
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import './index.sass';
 import Header from './components/header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <div>
-    <App />
     <Header />
   </div>,
   document.getElementById('root'));
-
 
 
 serviceWorker.unregister();
